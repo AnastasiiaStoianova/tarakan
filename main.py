94583863cf5644c339278b9ebe0489eda1c6f175
Anastasiia <asya@example.com>
2024-12-08 19:49:59 +0200

    Додано файл main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import poems
+import sign
+
+author_name = "Стоянова Настя"
+print(sign.HOMEWORK.format(name=author_name))
+
+wishes = "Побажання до курсу:\n- Більше практичних завдань\n- Приклади застосування у реальних проєктах"
+print(wishes)
+
+stars = "**************************************************************************"
+print(stars)
+
+print(poems.TESTAMENT)
+
+print(poems.THIRTEEN)
